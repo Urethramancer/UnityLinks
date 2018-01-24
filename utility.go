a5b1b0f67c68aa6e48b0fa0c59d90a211f765215
Ronny Bangsund <github@neuralshortcircuit.com>
2018-01-24 21:27:21 +0100

    Deleting dead code and preparing for some automation.

--- utility.go
+++ utility.go
@@ -15,23 +15,6 @@ func sane(s string) string {
 	return str.Clean(s)
 }
 
-// TODO: Find some criteria for cleanup of old versions.
-/*func initJanitor(s *Scrap) {
-	delay := s.Expiry - time.Now().Unix()
-	ticker := time.NewTicker(time.Duration(delay) * time.Second)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				ticker.Stop()
-				info("Janitor: Deleting %s", s.Hash)
-				s.delete()
-				return
-			}
-		}
-	}()
-}*/
-
 func fexists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
